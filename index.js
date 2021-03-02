983ebc6ffdfac92b00d2325e04f3dfaec07a3183
Lukas <77848587+lukascobit@users.noreply.github.com>
2021-03-02 17:48:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 function RandomCreator(){
     
-    let RandomNumber = Math.random() * 10;
-    let rr = Math.round(RandomNumber);
-
-
+   
     
 
     let letters = '0123456789ABCDEF';
@@ -19,11 +16,3 @@ function RandomCreator(){
 
     
 }
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    
-  }
